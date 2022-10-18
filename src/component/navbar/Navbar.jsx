b6201f69c7ef589620ef6218cdf1368b8cebff0e
fathcode99 <fath970106@gmail.com>
2022-10-18 20:28:23 +0700

    navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -10,25 +10,30 @@ const Navbar = () => {
   return (
     <div className='navbar h-12 border-b-2 border-slate-400 flex items-center text-sm text-slate-800'>
       <div className="wrapper p-5 flex items-center justify-between w-full">
-        <div className="search flex items-center border-2 border-slate-400 p-1 px-2 text-xs">
+        <div className="search flex items-center border-2 border-slate-400 p-1 px-2 text-sm">
           <input type="text" placeholder='Search ...' className='input outline-none bg-transparent' />
           <SearchIcon />
         </div>
-        <div className="items flex">
+        <div className="items flex items-center">
           <div className="item">
-            <DarkModeIcon />
+            <DarkModeIcon className="nav-icon" />
           </div>
           <div className="item">
-            <FullscreenExitIcon />
+            <FullscreenExitIcon className="nav-icon" />
           </div>
           <div className="item">
-            <NotificationsActiveIcon />
+            <NotificationsActiveIcon className="nav-icon" />
+            <div className="counter">1</div>
           </div>
           <div className="item">
-            <MarkChatUnreadIcon />
+            <MarkChatUnreadIcon className="nav-icon" />
+            <div className="counter">1</div>
           </div>
           <div className="item">
-            <FormatListBulletedIcon />
+            <FormatListBulletedIcon className="nav-icon" />
+          </div>
+          <div className="item">
+            <img className='avatar w-8 h-8 rounded-full' src="https://sp-images.summitpost.org/947006.jpg?auto=format&fit=max&h=800&ixlib=php-2.1.1&q=35&s=876696700800816d01e0d1eb31ce7ab0" alt="avatar" />
           </div>
         </div>
       </div>
